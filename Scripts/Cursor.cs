11ced75e1649d1673f33dfed1d49e084821169ed
raymittu <49529055+raymittu@users.noreply.github.com>
2020-07-19 19:31:34 +0200

    Script Final update
    Script Final update

---
+++ Scripts/Cursor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OpenCVForUnity.UnityUtils;
+using OpenCVForUnity.UnityUtils.Helper;
+using OpenCVForUnity.CoreModule;
+
+public class Cursor : MonoBehaviour
+{
+    public GameObject quad;
+    private float x;
+    private float y;
+    public int radius;
+    private bool isTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamTextureToMatHelper webCamTextureToMatHelper = quad.GetComponent<WebCamTextureToMatHelper>();
+        x = webCamTextureToMatHelper.requestedWidth / 2;
+        y = webCamTextureToMatHelper.requestedHeight /2;
+
+    }
+
+    public float Getx()
+    {
+        return x;
+    }
+    public float Gety()
+    {
+        return y;
+    }
+    public void SetisTrigger(bool _state)
+    {
+        isTrigger = _state;
+        this.gameObject.SetActive(isTrigger);
+    }
+
+}
---
+++ Scripts/StartEvent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartEvent : MonoBehaviour
+{
+   
+    public void StartApp(string _nameScene)
+    {
+        SceneManager.LoadScene(_nameScene);
+    }
+
+}
